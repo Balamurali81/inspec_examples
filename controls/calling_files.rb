fb1a1c7dc25215defc4d027935b6a0af813fe68a
Balamurali81 <70313343+Balamurali81@users.noreply.github.com>
2021-05-26 08:01:49 +0530

    modifed description

--- controls/calling_files.rb
+++ controls/calling_files.rb
@@ -1,11 +1,11 @@
-# example to call file that is loaded in the 'Files' folder of Inspec profile using inspec.profile.file
-#     /Controls/code.rb   -> in this example is used my file as Calling_files.rb
-#    /Files/filecontent.yml  -> in my code I used names.yml
+# example to call file that is loaded in the 'files' folder of Inspec profile using inspec.profile.file
+#     /Controls/code.rb   -> in this example code manifiest is in this file "Calling_files.rb"
+#    /files/filecontent.yml  -> in this example data input is from "names.yml"
 #  maintainted same file structure for convenience, you can refer the names.yml in the files folder
 
 control 'calling_files' do
-	title 'call the file from local machine and Inspec profile'
-	desc 'example to call file from local directory as well as from the 'Files' folder of Inspec profile using inspec.profile.file'
+	title 'Two scenarios to call the file from local machine and Inspec profile'
+	desc 'example to call file from local directory as well as from the files folder of Inspec profile using inspec.profile.file'
 
 # Scenario 1 for calling files from local machine	
 	if file('F:\Inpsec\names.yml').exist?                     # condition to check whether the file exists
